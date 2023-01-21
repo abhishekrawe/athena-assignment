b2263a93e265daa3a4be25f153978809d06eab6c
abhishekrawe <rajputabhisheksingh53@gmail.com>
2023-01-21 16:06:32 +0530

    Nav section with button fetch option from menuItem

---
+++ src/components/MenuItems.js
@@ -0,0 +1,20 @@
+const menuItems = [
+    {
+      title: "Product",
+      url: "#",
+      cName: "nav-links"
+    },
+    {
+      title: "Our Story",
+      url: "#",
+      cName: "nav-links"
+    },
+    {
+      title: "Resources",
+      url: "#",
+      cName: "nav-links"
+    }
+  ];
+  
+  export default menuItems;
+  
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import Button from "./Button";
+import menuItems from "./MenuItems";
+import "./Navbar.css";
+
+const Navbar = () => {
+  const [active, setActive] = useState(false);
+
+  const handleClick = () => {
+    setActive(!active);
+  };
+
+  return (
+    <nav className="navbar">
+      <h1 className="navbar-logo">
+        <img src="https://global-uploads.webflow.com/62efc7cb58ad153bfb146988/62efcbe40b52a66c04de66ce_Frame%2011.png" width="36"/>
+      </h1>
+      <div className="menu-icon" onClick={handleClick}>
+        <i className={active ? "fas fa-times" : "fas fa-bars"}></i>
+      </div>
+      <ul className={active ? "nav-menu active" : "nav-menu"}>
+        {menuItems.map((item, index) => {
+          return (
+            <li key={index}>
+              <a href={item.url} className={item.cName}>
+                {item.title}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <Button> Book a demo </Button>
+    </nav>
+  );
+};
+
+export default Navbar;
