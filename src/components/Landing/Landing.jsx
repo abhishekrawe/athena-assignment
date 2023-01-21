64cb5fb7e64077d1296dd73832a604749d57e318
abhishekrawe <rajputabhisheksingh53@gmail.com>
2023-01-22 02:25:31 +0530

    Worked on animated landing page

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,49 +1,65 @@
-import React from 'react'
+import { useEffect, useRef, useState } from "react";
 import './Landing.scss'
 
 const Landing = () => {
+
+    const [imgSource, setImgSrc] = useState("../Videos/video1.gif");
+    const leftDivRef = useRef(null);
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+    const handleScroll = (e) => {
+        const leftDiv = leftDivRef.current;
+        if (leftDiv) {
+            const scrollTop = 0 - leftDiv.getBoundingClientRect().top;
+            if (scrollTop < 500) {
+                setImgSrc("../Videos/video1.gif");
+            } else if (scrollTop < 800) {
+                setImgSrc("../Videos/video2.gif");
+            } else {
+                setImgSrc("../Videos/video3.gif");
+            }
+        }
+    };
     return (
         <div>
             <>
                 <div class="wrapper">
-                    <div class="main">
+                    <div class="main" ref={leftDivRef}>
                         <div class="automation-section">
-                        <div class="margin-bottom margin-xsmall">
-                            <div class="text-weight">
-                                <span class="text-span">Kula Outreach</span>
+                            <div class="margin-bottom margin-xsmall">
+                                <div class="text-weight">
+                                    <span class="text-span">Kula Outreach</span>
+                                </div>
                             </div>
+                            <h2 className='margin-xsmall'>Automated Candidate Follow-ups</h2>
+                            <p className='text-size-medium-2 margin-xsmall'>For prospective candidates, send automated, periodic follow-ups and value-adds to nurture long-term relationships</p>
                         </div>
-                        <h2 className='margin-xsmall'>Automated Candidate Follow-ups</h2>
-                        <p className='text-size-medium-2 margin-xsmall'>For prospective candidates, send automated, periodic follow-ups and value-adds to nurture long-term relationships</p>
-                        </div>
-
                         <div class="automation-section">
-                        <div class="margin-bottom margin-xsmall">
-                            <div class="text-weight">
-                                <span class="text-span">Kula Outreach</span>
+                            <div class="margin-bottom margin-xsmall">
+                                <div class="text-weight">
+                                    <span class="text-span">Kula Outreach</span>
+                                </div>
                             </div>
+                            <h2 className='margin-xsmall'>Personalized Candidate Reach Outs</h2>
+                            <p className='text-size-medium-2 margin-xsmall'>For prospective candidates, send automated, periodic follow-ups and value-adds to nurture long-term relationships</p>
                         </div>
-                        <h2 className='margin-xsmall'>Personalized Candidate Reach Outs</h2>
-                        <p className='text-size-medium-2 margin-xsmall'>For prospective candidates, send automated, periodic follow-ups and value-adds to nurture long-term relationships</p>
-                        </div>
-
-
                         <div class="automation-section">
-                        <div class="margin-bottom margin-xsmall">
-                            <div class="text-weight">
-                                <span class="text-span">Kula Outreach</span>
+                            <div class="margin-bottom margin-xsmall">
+                                <div class="text-weight">
+                                    <span class="text-span">Kula Outreach</span>
+                                </div>
                             </div>
+                            <h2 className='margin-xsmall'>Reach out on channels candidates prefer</h2>
+                            <p className='text-size-medium-2 margin-xsmall'>For prospective candidates, send automated, periodic follow-ups and value-adds to nurture long-term relationships</p>
                         </div>
-                        <h2 className='margin-xsmall'>Reach out on channels candidates prefer</h2>
-                        <p className='text-size-medium-2 margin-xsmall'>For prospective candidates, send automated, periodic follow-ups and value-adds to nurture long-term relationships</p>
-                        </div>
-                        
                     </div>
-
                     <div class="sidebar">
-                    <iframe className="video" src="../Videos/video1.mp4" frameborder="0"></iframe>
+                        <img className="img" src={imgSource} alt="placeholder" />
                     </div>
-                    
                 </div>
             </>
         </div>
