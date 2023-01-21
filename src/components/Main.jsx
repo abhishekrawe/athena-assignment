a23c59165aeed9fb2db2abb105ae548bdb723a11
abhishekrawe <rajputabhisheksingh53@gmail.com>
2023-01-21 16:07:01 +0530

    main title of the section

---
+++ src/components/Main.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Main.css'
+function Main() {
+
+    return (
+        <>
+            <div class="container-s"><div class="container-ver">
+                <div class="text"><div class="max-w align-center">
+                    <div class="margin-bottom margin-small">
+                        <h1 > Double the hires, half the effort<br /></h1>
+                    </div>
+                    <p class="text-size">Open conversations and nurture relationships at scale and be the first choice when your ideal candidate is ready to explore.<br /></p>
+                    <div class="margin-top margin-medium">
+                        <div class="btn-row">
+                            <a href="" class="btn-t btn">View Kula Outreach</a>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </>
+    )
+}
+
+export default Main;
