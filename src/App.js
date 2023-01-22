9998e67e0d33d6ddd46ab3d10f9a7bf7d79e5c3f
abhishekrawe <rajputabhisheksingh53@gmail.com>
2023-01-22 17:34:04 +0530

    ❄️Fetch API & render Data to Landing Page Section

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from './components/Navbar';
-import Main from "./components/Main";
+import Header from "./components/Header";
 import './App.css';
 import Landing from "./components/Landing/Landing";
 function App() {
+
+  //https://mocki.io/v1/ee762599-31ae-4a3d-a6c7-d596525945e1
+
+  const [text, setText] = useState([]);
+  console.log(text);
+
+  useEffect(() => {
+    textapi();
+  }, [])
+
+  const textapi = async () => {
+    const response = await fetch("https://mocki.io/v1/ee762599-31ae-4a3d-a6c7-d596525945e1");
+    //console.log(response) ;
+    const jsonData = await response.json();
+    //console.log(jsonData);
+    setText(jsonData.texts);
+  }
+
+  //textapi();
+
+
   return (
-    <div className="App">
-       <Navbar/>
-       <Main/>
-       <Landing/>
-    </div>
+    <>
+    <Navbar />
+    <Header text={text}/>
+    <Landing text={text}/>
+    </>
+
   );
 }
 
